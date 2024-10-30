d85184024d2764c931abd609d319fbe382eee2a9
Hung trinh <hungbuit@gmail.com>
2024-10-30 13:26:26 +0700

    showTopicSelectionBottomSheet remove  list topcs learn complete

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,11 @@
-import 'package:bvo/model/word.dart';
-import 'package:bvo/repository/word_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart'; // For CupertinoIcons
+
+import 'package:bvo/model/word.dart';
+import 'package:bvo/model/topic.dart';
+import 'package:bvo/repository/word_repository.dart';
+import 'package:bvo/repository/topic_repository.dart';
+import 'package:bvo/screen/topic_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,17 +16,12 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Map<String, List<Word>> reviewedWordsByTopic = {};
+  List<Topic> topics = [];
 
   @override
   void initState() {
     super.initState();
     init();
-  }
-
-  Future<void> init() async {
-    reviewedWordsByTopic =
-        await WordRepository().getReviewedWordsGroupedByTopic();
-    setState(() {});
   }
 
   @override
@@ -32,16 +31,17 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("Box of Your Vocabulary"),
       ),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // navigator or show topic screen to choose topic screen
-          },
-          backgroundColor: Colors.orange,
-          foregroundColor: Colors.white,
-          shape: const CircleBorder(
-            side: BorderSide(color: Colors.orange, width: 1),
-          ),
-          tooltip: 'Add words',
-          child: const Icon(Icons.add)),
+        onPressed: () {
+          showTopicSelectionBottomSheet();
+        },
+        backgroundColor: Colors.orange,
+        foregroundColor: Colors.white,
+        shape: const CircleBorder(
+          side: BorderSide(color: Colors.orange, width: 1),
+        ),
+        tooltip: 'Add words',
+        child: const Icon(Icons.add),
+      ),
       body: reviewedWordsByTopic.isEmpty
           ? const Center(
               child: Text(
@@ -76,7 +76,7 @@ class _HomeScreenState extends State<HomeScreen> {
             itemCount: words.length,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2, // 2 items per row
-              childAspectRatio: 2.5, // Increase this value to reduce height
+              childAspectRatio: 2.5, // Adjust as needed
               mainAxisSpacing: 4.0,
               crossAxisSpacing: 4.0,
             ),
@@ -101,7 +101,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              const SizedBox(width: 16.0),
+              const SizedBox(width: 8.0),
               Expanded(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
@@ -111,15 +111,15 @@ class _HomeScreenState extends State<HomeScreen> {
                       word.en,
                       style: const TextStyle(
                         fontWeight: FontWeight.bold,
-                        fontSize: 16,
+                        fontSize: 14,
                       ),
-                      textAlign: TextAlign.center,
+                      textAlign: TextAlign.left,
                     ),
-                    const SizedBox(height: 4.0),
+                    const SizedBox(height: 2.0),
                     Text(
                       word.vi,
-                      style: const TextStyle(color: Colors.grey),
-                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.grey, fontSize: 12),
+                      textAlign: TextAlign.left,
                     ),
                   ],
                 ),
@@ -140,8 +140,107 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ],
               ),
-              const SizedBox(width: 16.0),
+              const SizedBox(width: 8.0),
             ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> init() async {
+    reviewedWordsByTopic =
+        await WordRepository().getReviewedWordsGroupedByTopic();
+    topics = await TopicRepository().getTopics(); // Load topics
+    setState(() {});
+  }
+
+  void showTopicSelectionBottomSheet() {
+    // Identify topics already on the homepage
+    Set<String> reviewedTopics = reviewedWordsByTopic.keys.toSet();
+
+    // Filter out these topics from the topics list
+    List<Topic> availableTopics =
+        topics.where((topic) => !reviewedTopics.contains(topic.topic)).toList();
+
+    // Check if there are any available topics
+    if (availableTopics.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('All topics have been added.')),
+      );
+      return;
+    }
+
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16.0)),
+      ),
+      isScrollControlled: true,
+      builder: (context) {
+        return SizedBox(
+          height:
+              MediaQuery.of(context).size.height * 0.75, // Set desired height
+          child: Column(
+            children: [
+              // Title of the bottom sheet
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Choose Topic',
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              // Divider (optional)
+              const Divider(height: 1, thickness: 1),
+              // Expanded widget to contain the GridView
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: GridView.builder(
+                    itemCount: availableTopics.length,
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2, // 2 cards per row
+                      childAspectRatio: 3 / 2, // Adjust as needed
+                      mainAxisSpacing: 4.0,
+                      crossAxisSpacing: 4.0,
+                    ),
+                    itemBuilder: (context, index) {
+                      final topic = availableTopics[index];
+                      return topicWidget(topic: topic.topic);
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget topicWidget({required String topic}) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pop(context); // Close the bottom sheet
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => TopicScreen(topic: topic)),
+        );
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Center(
+            child: Text(
+              topic,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
           ),
         ),
       ),
