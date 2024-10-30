263aae4aa873e8efc66027af3b87c597e77f8b82
Hung trinh <hungbuit@gmail.com>
2024-10-30 13:13:30 +0700

    tmp

--- lib/repository/word_repository.dart
+++ lib/repository/word_repository.dart
@@ -1,8 +1,10 @@
 // word repository class
 import 'dart:convert';
 
+import 'package:bvo/model/topic.dart';
 import 'package:bvo/model/word.dart';
 import 'package:bvo/repository/dictionary.dart';
+import 'package:bvo/repository/topic_repository.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class WordRepository {
@@ -43,5 +45,30 @@ class WordRepository {
     List<String> wordsJson =
         words.map((word) => jsonEncode(word.toJson())).toList();
     await prefs.setStringList('words_$topic', wordsJson);
+  }
+
+  Future<Map<String, List<Word>>> getReviewedWordsGroupedByTopic() async {
+    List<Topic> topics = await TopicRepository().getTopics();
+    Map<String, List<Word>> reviewedWordsByTopic = {};
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    for (Topic topic in topics) {
+      List<String>? savedWordsJson =
+          prefs.getStringList('words_${topic.topic}');
+      if (savedWordsJson != null) {
+        List<Word> words = savedWordsJson
+            .map((wordJson) => Word.fromJson(jsonDecode(wordJson)))
+            .toList();
+
+        List<Word> reviewedWords =
+            words.where((word) => word.reviewCount > 0).toList();
+
+        if (reviewedWords.isNotEmpty) {
+          reviewedWordsByTopic[topic.topic] = reviewedWords;
+        }
+      }
+    }
+    return reviewedWordsByTopic;
   }
 }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,7 +1,7 @@
-import 'package:bvo/model/topic.dart';
-import 'package:bvo/repository/topic_repository.dart';
-import 'package:bvo/screen/topic_screen.dart';
+import 'package:bvo/model/word.dart';
+import 'package:bvo/repository/word_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart'; // For CupertinoIcons
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,75 +11,140 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Topic> topics = [];
+  Map<String, List<Word>> reviewedWordsByTopic = {};
 
   @override
   void initState() {
     super.initState();
-
     init();
+  }
+
+  Future<void> init() async {
+    reviewedWordsByTopic =
+        await WordRepository().getReviewedWordsGroupedByTopic();
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: const Text("Bun Vocabulary"),
+        title: const Text("Box of Your Vocabulary"),
       ),
-      body: GridView.builder(
-          itemCount: topics.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, // Maximum 2 cards per row
-            childAspectRatio: 3 / 2, // Adjust the aspect ratio as needed
-            mainAxisSpacing: 4.0, // Space between rows
-            crossAxisSpacing: 4.0, // Space between columns
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // navigator or show topic screen to choose topic screen
+          },
+          backgroundColor: Colors.orange,
+          foregroundColor: Colors.white,
+          shape: const CircleBorder(
+            side: BorderSide(color: Colors.orange, width: 1),
           ),
-          padding: const EdgeInsets.all(4.0),
-          itemBuilder: (context, index) {
-            final word = topics[index];
-            return topicWidget(topic: word.topic);
-          }),
+          tooltip: 'Add words',
+          child: const Icon(Icons.add)),
+      body: reviewedWordsByTopic.isEmpty
+          ? const Center(
+              child: Text(
+                'No words have been reviewed yet.',
+                style: TextStyle(fontSize: 18),
+              ),
+            )
+          : ListView.builder(
+              itemCount: reviewedWordsByTopic.length,
+              itemBuilder: (context, index) {
+                String topic = reviewedWordsByTopic.keys.elementAt(index);
+                List<Word> words = reviewedWordsByTopic[topic]!;
+                return topicSection(topic, words);
+              },
+            ),
     );
   }
 
-  Widget topicWidget({required String topic}) {
-    return GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => TopicScreen(topic: topic)),
-          );
-        },
-        child: Card(
-          child: Padding(
-            padding: const EdgeInsets.all(4.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  topic,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-              ],
+  Widget topicSection(String topic, List<Word> words) {
+    return ExpansionTile(
+      initiallyExpanded: true,
+      title: Text(
+        topic,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: GridView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: words.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, // 2 items per row
+              childAspectRatio: 2.5, // Increase this value to reduce height
+              mainAxisSpacing: 4.0,
+              crossAxisSpacing: 4.0,
             ),
+            itemBuilder: (context, index) {
+              final word = words[index];
+              return wordCard(word);
+            },
           ),
-        ));
+        ),
+      ],
+    );
   }
 
-  init() async {
-    topics = await TopicRepository().getTopics();
-    setState(() {});
+  Widget wordCard(Word word) {
+    return GestureDetector(
+      onTap: () {
+        // Optionally, navigate to a detail screen for the word
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(1.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(width: 16.0),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      word.en,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const SizedBox(height: 4.0),
+                    Text(
+                      word.vi,
+                      style: const TextStyle(color: Colors.grey),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 8.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    CupertinoIcons.heart_fill,
+                    size: 16,
+                    color: Colors.deepPurpleAccent,
+                  ),
+                  const SizedBox(width: 4),
+                  Text(
+                    "${word.reviewCount}",
+                    style: const TextStyle(fontSize: 13),
+                  ),
+                ],
+              ),
+              const SizedBox(width: 16.0),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
